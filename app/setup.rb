6bf046f53a263f6883501f36af9ebdd074e014b1
jphager2 <jphager2@gmail.com>
2016-07-13 18:06:22 +0200

    get rid of dirty loging

--- app/setup.rb
+++ app/setup.rb
@@ -5,27 +5,6 @@ require 'active_record'
 require 'ruby-go'
 require 'letter_opener'
 
-module JohnsHelper
-  def self.print_config(config, logger, indent = 0)
-    Array(config).each do |name, value|
-      if inner_config = value.instance_variable_get(:@envs)
-        logger.info name.to_s
-        inner_config = inner_config[:production].instance_variable_get(:@opts)
-        defaults = value.defaults.instance_variable_get(:@opts)
-        if inner_config
-          opts = defaults.merge inner_config
-        else
-          opts = defaults
-        end
-        print_config(opts, logger, indent)
-      else
-        logger.info "#{" " * indent}#{name}: #{value}"
-      end
-    end
-    puts
-  end
-end
-
 Pakyow::App.define do
   configure :global do
     # put global config here and they'll be available across environments
@@ -65,11 +44,6 @@ Pakyow::App.define do
     realtime.redis = { url: "redis://h:p7vo203cje5f5o48slhgm2ds1ko@ec2-54-235-76-229.compute-1.amazonaws.com:6909" }
 
     $db = ActiveRecord::Base.establish_connection
-  end
-
-  Pakyow::App.after(:configure) do
-    config = Pakyow::App.config.instance_variable_get(:@config)
-    JohnsHelper.print_config(config, Pakyow.logger)
   end
 end
 
