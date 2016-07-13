95c65ba9368663c85c3f151f06ce1f568baafff5
jphager2 <jphager2@gmail.com>
2016-07-13 20:40:35 +0200

    logging redis

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -1,6 +1,7 @@
 Pakyow::App.routes do
 
   default '/' do
+    Pakyow.logger.info "[Config][Realtime][Redis] #{Pakyow.app.config.realtime.redis}"
     # Replace game.board.board with persisted board
     # Replace game.moves with with persited moves
     @pgame ||= current_game
